2e90663d045f1f2337c48f263b662754b415bb7a
Pauline <mya@mya-ow.net>
2018-07-17 15:58:17 +0200

    modifs game.rb

--- game.rb
+++ game.rb
@@ -3,10 +3,9 @@ require_relative "player"
 
 class Game 
 
-  attr_accessor :rules, :ending, :turn
+  attr_accessor :ending, :turn
 
   def initialize(rules)
-    @rules = rules                  # initialise les règles du jeu
     @ending = false                 # initialise la fin du jeu
     @turn = Random.rand(0..1)       # choisit le premier joueur 
     @players = []                   # initialise et récupère les noms des joueurs
@@ -35,7 +34,9 @@ class Game
     puts "Tape 2 pour afficher les règles"
     puts "Tape 3 pour afficher les cases"
     puts "Tape 4 pour sortir du jeu"
+
     i = gets.chomp.to_i
+
     if i == 1
       9.times {
 
@@ -50,20 +51,28 @@ class Game
         problem = @board.update_case(cases.to_i - 1, @players[@turn].symbol)  # update le fichier board avec les valeurs des cases
         
          @board.display
+
       if problem == 1
         altern
+      
       else
         puts "Cette case est déjà prise, essaie encore !"
+        
       end
     }
+
     elsif i == 2
       puts "Il y a deux joueurs; l’un joue avec le signe x et l’autre avec le signe o. Les deux joueurs remplissent alternativement les cellules vides. Au début du jeu, il y a 9 cellules vides dans un panneau 3x3. Le but est de placer 3 signes identiques sur une colonne, une ligne ou une diagonale. Le jeu prend fin si le panneau est entièrement rempli et si aucun joueur n’arrive à atteindre le but. Bonne chance !\n\n"
+    
     elsif i == 3
-      @board.show_index
+      @board.show_index            # affiche le tableau avec le nom des cases
       puts "\n"
+    
     elsif i == 4
+    
     else 
       puts "Mauvaise commande, recommence !"
+
     end 
   end while i != 4
 
