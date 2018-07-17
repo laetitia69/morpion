dabbc346e6e5d0334e811cc9f84d01c17d720879
willy <wilg1569@gmail.com>
2018-07-17 15:52:49 +0200

    Ajout fonvtion game ending

--- board.rb
+++ board.rb
@@ -35,4 +35,29 @@ class Board
 		puts "#{@cases[3] = 4}|#{@cases[4] = 5}|#{@cases[5] = 6}"
 		puts "#{@cases[6] = 7}|#{@cases[7] = 8}|#{@cases[8] = 9}"
 	end
+
+	def game_ending
+		# verifier si joueur avec symbol X a gagne
+		  @victory1 = @cases[0] == "x" && @cases[1] == "x" && @cases[2] == "x" || #verifie une victoire Horizontale pour
+   		 @cases[3] == "x" && @cases[4] == "x" && @cases[5] == "x" ||
+   		 @cases[6] == "x" && @cases[7] == "x" && @cases[8] == "x" ||
+
+   		 @cases[0] == "x" && @cases[3] == "x" && @cases[6] == "x" || #verfier une victoire verticale pour
+       @cases[1] == "x" && @cases[4] == "x" && @cases[7] == "x" ||
+       @cases[2] == "x" && @cases[5] == "x" && @cases[8] == "x" ||
+
+       @cases[0] == "x" && @cases[4] == "x" && @cases[8] == "x" || #verifier una victoire diagonale
+       @cases[2] == "x" && @cases[4] == "x" && @cases[6] == "x"
+
+ 		   @victory2 = @cases[0] == "o" && @cases[1] == "o" && @cases[2] == "o" ||
+       @cases[3] == "o" && @cases[4] == "o" && @cases[5] == "o" ||
+       @cases[6] == "o" && @cases[7] == "o" && @cases[8] == "o" ||
+
+       @cases[0] == "o" && @cases[3] == "o" && @cases[6] == "o" ||
+       @cases[1] == "o" && @cases[4] == "o" && @cases[7] == "o" ||
+       @cases[2] == "o" && @cases[5] == "o" && @cases[8] == "o" ||
+
+       @cases[0] == "o" && @cases[4] == "o" && @cases[8] == "o" ||
+       @cases[2] == "o" && @cases[4] == "o" && @cases[6] == "o"
+	end
 end
