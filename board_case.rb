4240e9ca61d6d91d828b7798cf49a9280dbba474
loundness <ludo.vic@me.com>
2018-07-17 13:47:04 +0200

    modification du code

--- board_case.rb
+++ board_case.rb
@@ -1,9 +1,28 @@
+
 class BoardCase
-	attr_accessor :symbol, :case_index, :case_status
+	attr_reader :symbol 
 	#definit l'état de boardcase à l'initialisation
-	def initialize(index) #index variable qui vient de la méthode de la classe board
-		@symbol = "" #case vide à l'initialisation du jeu en attente de symbole X, O, ""
-		@case_index = index #position de la case suivant les valeurs d'index
-		@case_status = false #les cases du morpion sont vides à l'initialisation
+	def initialize #index variable qui vient de la méthode de la classe board
+		@symbol = "" #case vide à l'initialisation du jeu en attente de symbole X, O
+	
 	end
+
+	def update_symbol(symbol)
+		if @symbol == "" #verifie s'il y a un symbole ou pas
+			
+			@symbol = symbol #pour afficher le symbole
+			return 1
+		else  
+			return 0	
+		end	
+	end
+		def is_empty #si la case est vide
+			return @symbol == "" #renvoie true si vide ou false si presence symbole
+		end	
 end		
+
+
+
+
+
+
