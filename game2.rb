4fe5737176b360c5f8dac34f2b1fbb0b6f718775
jihane <thp@jihane.fr>
2018-07-17 12:21:40 +0200

    test

--- game2.rb
+++ game2.rb
@@ -32,5 +32,51 @@ class Game
 end
 
 
-game = Game.new
-game.go
+#game = Game.new
+#game.go
+
+bc = BoardCase.new(1)
+puts bc.symbol
+-------------
+    
+
+
+
+require_relative "board_case"
+
+class Board
+	#include Enumerable
+	#attributs getter et setter réunis
+	attr_accessor :cases
+
+	
+	def initialize
+		@cases = Array.new(9)#création de l'array 
+
+		#on rempli le tableau 9 fois par la class BoardCases qui définit une seule case
+		9.times {|i|
+			@cases.push(BoardCase.new(i))
+		}
+        
+        @cases.each{
+            |x| puts x
+            }
+        
+        
+	end
+
+	#Afficher le quadrillage
+	def display
+        
+        @cases.each{
+            |x| puts x
+            }
+		#print @cases[0..2]
+		#print @cases[3..5]
+		#print @cases[6..8]
+	end
+
+end
+
+board = Board.new()
+board.display
